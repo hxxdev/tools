f93c5a4eef131b73a763194bf1f2bfb59ffcfea4
hxx.dev <hxx.dev@gmail.com>
2025-05-24 18:50:37 +0900

    shortkeys for wezterm panes.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,9 +16,66 @@ config.font_size = 10
 config.color_scheme = "Catppuccin Mocha"
 config.text_background_opacity = 0.3
 config.window_background_opacity = 0.5
-config.window_background_image = '/path/to/image'
+config.window_background_image = '\\\\wsl.localhost\\Ubuntu-24.04\\home\\hxxdev\\dev\\tools\\wallpapers\\jp.png'
 
 config.default_prog = { "wsl.exe" }
 config.font = wezterm.font("Hack Nerd Font", { weight = "Regular", italic = false })
 -- Finally, return the configuration to wezterm:
+config.keys = {
+    {
+      key = '"',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '%',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'x',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+      key = 'h',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.AdjustPaneSize { 'Left', 5 },
+    },
+    {
+      key = 'l',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.AdjustPaneSize { 'Right', 5 },
+    },
+    {
+      key = 'k',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.AdjustPaneSize { 'Up', 2 },
+    },
+    {
+      key = 'j',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.AdjustPaneSize { 'Down', 2 },
+    },
+    {
+      key = 'h',
+      mods = 'ALT',
+      action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'l',
+      mods = 'ALT',
+      action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'k',
+      mods = 'ALT',
+      action = wezterm.action.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'j',
+      mods = 'ALT',
+      action = wezterm.action.ActivatePaneDirection 'Down',
+    },
+  }
 return config
