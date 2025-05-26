19d9a568f79175c7abb41a41e9bb3939a01c1c30
hxx.dev <hxx.dev@gmail.com>
2025-05-26 23:22:08 +0900

    fix bug:mount.sh

--- wezterm/docker/mount.sh
+++ wezterm/docker/mount.sh
@@ -1,4 +1,4 @@
 docker run \
   -v $(pwd)/out:/out \
-  wezterm-builder \
-  cp /usr/local/bin/wezterm* /out/ | cp /usr/local/bin/strip-ansi-escapes /out/
+  wezterm-centos:latest \
+  /bin/bash -c "cp /usr/local/bin/wezterm* /out/ | cp /usr/local/bin/strip-ansi-escapes /out/"
