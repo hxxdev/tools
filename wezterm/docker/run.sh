0599f718a3b434e181d40b4af1e378e62b534311
hxx.dev <hxx.dev@gmail.com>
2025-05-26 23:23:15 +0900

    run.sh refactoring

--- wezterm/docker/run.sh
+++ wezterm/docker/run.sh
@@ -1,5 +1,7 @@
+xhost +local:docker  # allow X11
+
 docker run -it \
   --env DISPLAY=$DISPLAY \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
-  wezterm-centos \
+  wezterm-centos:latest \
   /usr/local/bin/wezterm
